f0298272144d2155db1447a0fbd629c272bd8a0a
Sushant Thyagaraj <sushantt@gmail.com>
2023-04-13 20:24:34 -0700

    Initial commit.

---
+++ FridAI/FridAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  frid_aiApp.swift
+//  frid-ai
+//
+//  Created by Sushant Thyagaraj on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct frid_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
