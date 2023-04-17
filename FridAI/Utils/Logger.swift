1673589005fcd5014eea5c342cc7df94cc910914
Sushant Thyagaraj <sushantt@gmail.com>
2023-04-16 18:57:32 -0700

    Implemented global logger module as a Singleton.

--- FridAI/Utils/Logger.swift
+++ FridAI/Utils/Logger.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+class Logger: ObservableObject {
+    static let shared = Logger()
+
+    @Published var messageLog = ""
+    private init() {}
+
+    func addLog(msg: String) {
+        messageLog += msg
+    }
+}
